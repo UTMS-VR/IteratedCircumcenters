cc3e1351c4dbddd76bb6c8c77ac0e53a38e77889
1123sodium <oudism1123_4772930@icloud.com>
2024-06-15 17:01:32 +0900

    debug

--- Assets/Scripts/Circumcenter.cs
+++ Assets/Scripts/Circumcenter.cs
@@ -1,38 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Circumcenter
 {
-    public static float[3] circumcenter3d(float[3] x, y, z, w)
+    public static float[] circumcenter3d(float[] x, float[] y, float[] z, float[] w)
     {
-        float[3, 3] mat = {
-            {y[0] - x[0], y[1] - x[1], y[2] - x[2]},
-            {z[0] - x[0], z[1] - x[1], z[2] - x[2]},
-            {w[0] - x[0], w[1] - x[1], w[2] - x[2]},
-            };
-        float det = MatrixCalculation.determinant(matrix);
-        float[3] center;
+        float[][] mat = new float[3][]
+        {
+            new float[] {y[0] - x[0], y[1] - x[1], y[2] - x[2]},
+            new float[] {z[0] - x[0], z[1] - x[1], z[2] - x[2]},
+            new float[] {w[0] - x[0], w[1] - x[1], w[2] - x[2]},
+        };
+        float det = MatrixCalculation.determinant(mat);
+        float[] center;
         if (Math.Abs(det) < 0.000001f)
         {
-            center = { Single.MaxValue, Single.MaxValue, Single.MaxValue};
+            center = new float[3] { Single.MaxValue, Single.MaxValue, Single.MaxValue };
         }
         else
         {
-            float[3] mattp = MatrixCalculation.transpose(mat);
-            float[3, 3] b = MatrixCalculation.matmul(mat, mattp);
-            float[3, 3] a = MatrixCalculation.inverse(b);
-            float[3] sizesq = {
+            float[][] mattp = MatrixCalculation.transpose(mat);
+            float[][] b = MatrixCalculation.matmul(mat, mattp);
+            float[][] a = MatrixCalculation.inverse(b);
+            float[] sizesq = {
                     MatrixCalculation.dotmul(mat[0], mat[0]),
                     MatrixCalculation.dotmul(mat[1], mat[1]),
                     MatrixCalculation.dotmul(mat[2], mat[2])
                 };
-            float[3] par = {
+            float[] par = {
                     MatrixCalculation.dotmul(a[0],sizesq)/2,
                     MatrixCalculation.dotmul(a[1],sizesq)/2,
                     MatrixCalculation.dotmul(a[2],sizesq)/2
                 };
-            center = {
+            center = new float[3] {
                 MatrixCalculation.dotmul(mattp[0], par) + x[0],
                 MatrixCalculation.dotmul(mattp[1], par) + x[1],
                 MatrixCalculation.dotmul(mattp[2], par) + x[2]
--- Assets/Scripts/MatrixCalculation.cs
+++ Assets/Scripts/MatrixCalculation.cs
@@ -1,84 +1,90 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MatrixCalculation
 {
-    public static float determinant(float[3, 3] a)
+    public static float determinant(float[][] a)
     {
-        float det = a[1, 1] * a[2, 2] * a[0, 0]
-                    + a[1, 2] * a[2, 0] * a[0, 1]
-                    + a[1, 0] * a[2, 1] * a[0, 2]
-                    - a[1, 1] * a[2, 0] * a[0, 2]
-                    - a[1, 2] * a[2, 1] * a[0, 0]
-                    - a[1, 0] * a[2, 2] * a[0, 1];
+        float det = a[1][1] * a[2][2] * a[0][0]
+                    + a[1][2] * a[2][0] * a[0][1]
+                    + a[1][0] * a[2][1] * a[0][2]
+                    - a[1][1] * a[2][0] * a[0][2]
+                    - a[1][2] * a[2][1] * a[0][0]
+                    - a[1][0] * a[2][2] * a[0][1];
         return det;
     }
 
-    public static float[3, 3] inverse(float[3, 3] a)
+    public static float[][] inverse(float[][] a)
     {
         float det = determinant(a);
-        float[3, 3] inv;
+        float[][] inv;
         if (Math.Abs(det) < 0.000001f)
         {
-            inv = {
-                { Single.MaxValue,Single.MaxValue,Single.MaxValue},
-                { Single.MaxValue,Single.MaxValue,Single.MaxValue},
-                { Single.MaxValue,Single.MaxValue,Single.MaxValue}
+            inv = new float[3][]
+            {
+                new float[] { Single.MaxValue,Single.MaxValue,Single.MaxValue},
+                new float[] { Single.MaxValue,Single.MaxValue,Single.MaxValue},
+                new float[] { Single.MaxValue,Single.MaxValue,Single.MaxValue}
             };
         }
         else
         {
-            inv ={
+            inv = new float[3][] 
+            {
+                new float[]
                 {
-                    (a[1, 1] * a[2, 2] - a[1, 2] * a[2, 1]) / det,
-                    (a[0, 2] * a[2, 1] - a[0, 1] * a[2, 2]) / det,
-                    (a[0, 1] * a[1, 2] - a[0, 2] * a[1, 1]) / det
+                    (a[1][1] * a[2][2] - a[1][2] * a[2][1]) / det,
+                    (a[0][2] * a[2][1] - a[0][1] * a[2][2]) / det,
+                    (a[0][1] * a[1][2] - a[0][2] * a[1][1]) / det
                 },
+                new float[]
                 {
-                    (a[1, 2] * a[2, 0] - a[1, 0] * a[2, 2]) / det,
-                    (a[0, 0] * a[2, 2] - a[0, 2] * a[2, 0]) / det,
-                    (a[0, 2] * a[1, 0] - a[0, 0] * a[1, 2]) / det
+                    (a[1][2] * a[2][0] - a[1][0] * a[2][2]) / det,
+                    (a[0][0] * a[2][2] - a[0][2] * a[2][0]) / det,
+                    (a[0][2] * a[1][0] - a[0][0] * a[1][2]) / det
                 },
+                new float[]
                 {
-                    (a[1, 0] * a[2, 1] - a[1, 1] * a[2, 0]) / det,
-                    (a[0, 1] * a[2, 0] - a[0, 0] * a[2, 1]) / det,
-                    (a[0, 0] * a[1, 1] - a[0, 1] * a[1, 0]) / det
+                    (a[1][0] * a[2][1] - a[1][1] * a[2][0]) / det,
+                    (a[0][1] * a[2][0] - a[0][0] * a[2][1]) / det,
+                    (a[0][0] * a[1][1] - a[0][1] * a[1][0]) / det
                 }
-            }
+            };
         };
         return inv;
     }
 
-    public static float[3, 3] transpose(float[3, 3] a)
+    public static float[][] transpose(float[][] a)
     {
-        float[3, 3] tp;
+        float[][] tp = new float[3][];
 
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                tp[j, i] = a[i, j];
+                tp[j][i] = a[i][j];
             }
         }
         return tp;
     }
 
-    public static float[3] dotmul(float[3] a, b)
+    public static float dotmul(float[] a, float[] b)
     {
         float ans = a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
         return ans;
     }
 
-    public static float[3, 3] matmul(float[3, 3] a, b)
+    public static float[][] matmul(float[][] a, float[][] b)
     {
-        float[3, 3] ans;
-        float[3, 3] bt = transpose(b)
+        float[][] ans = new float[3][];
+        float[][] bt = transpose(b);
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                ans[i, j] = dotmul(a[i], bt[j]);
+                ans[i][j] = dotmul(a[i], bt[j]);
             }
         }
         return ans;
